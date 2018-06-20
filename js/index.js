7d6d3fd0f7d60230a48f8a3f0bdf70c2b5e88b74
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-06-20 15:40:27 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 $('.menu-container').click(function() {
   $(this).toggleClass('opened');
 });
+
+//ScrollReveal - efekt wjeżdzających napisów
+    window.sr = ScrollReveal();
+    sr.reveal('.banner', {
+       origin: 'left', 
+       distance: '50vw', 
+       duration: 1000, 
+       viewFactor: 0.5
+    });
+    /*sr.reveal('h2, p', {
+        reset: true,
+        delay: 300
+    });*/

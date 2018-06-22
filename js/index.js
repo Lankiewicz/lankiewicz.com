510fd0c9cd18c440253497d61fb5ad438121cb8f
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-06-22 16:55:17 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
-$('.menu-container').click(function() {
-  $(this).toggleClass('opened');
-});
-
 //ScrollReveal - efekt wjeżdzających napisów
     window.sr = ScrollReveal();
     sr.reveal('.banner',{
        origin: 'left', 
        distance: '50vw', 
        duration: 1000, 
+       viewFactor: 0.5 
+    });
+       sr.reveal('.header-about-me',{
+       origin: 'left', 
+       distance: '50vw', 
+       duration: 1000, 
        viewFactor: 0.5
     });
+       sr.reveal('p',{
+       origin: 'top', 
+       distance: '50vw', 
+       duration: 3300, 
+       viewFactor: 0.5
+    });
+$('.menu-container').click(function() {
+  $(this).toggleClass('opened');
+});
 
-$("#fullpage").fullpage();
+
 
